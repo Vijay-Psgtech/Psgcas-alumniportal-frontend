1d2a7e9544ce49714fda7a42da01fc4f487c4dd5
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-03-02 09:38:53 +0530

    Auth Event handler component has been added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import ScrolltoTop from "./components/ScrolltoTop";
+import AuthEventHandler from "./components/AuthEventHandler";
 
 import AlumniRegistration from "./pages/alumni/AlumniRegistration";
 import AlumniLogin from "./pages/alumni/AlumniLogin";
@@ -48,6 +49,7 @@ const AppLoader = () => (
 function AppRoutes() {
   return (
     <>
+      <AuthEventHandler />
       <ScrolltoTop />
       <Routes>
        
